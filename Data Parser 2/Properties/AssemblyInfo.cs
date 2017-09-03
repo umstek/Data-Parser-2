fbd85db7cf79135e9cf5bcdd067c1b1aba7227d7
Wickramaranga Abeygunawardhana <umstek@live.com>
2017-09-03 18:21:40 +0530

    Perform cleanup

--- Data Parser 2/Properties/AssemblyInfo.cs
+++ Data Parser 2/Properties/AssemblyInfo.cs
@@ -8,10 +8,10 @@ using System.Windows;
 [assembly: AssemblyTitle("Data Parser 2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("UMSTeK")]
 [assembly: AssemblyProduct("Data Parser 2")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © UMSTeK 2017")]
+[assembly: AssemblyTrademark("UMSTeK™")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
